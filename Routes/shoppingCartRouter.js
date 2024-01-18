a8be3d02f6d7b02c612bb05bdd2956bbec4d5256
John C <incarnate@users.noreply.github.com>
2024-01-19 10:37:54 +1100

    Improvements and bugfixes

--- Routes/shoppingCartRouter.js
+++ Routes/shoppingCartRouter.js
@@ -3,7 +3,7 @@ const shoppingCartRouter = express.Router();
 
 const shoppingCartController = require('../controller/shoppingCartController');
 
-shoppingCartRouter.get('/shoppingCart', shoppingCartController.renderShoppingCart);
+shoppingCartRouter.post('/shoppingCart', shoppingCartController.renderShoppingCart);
 shoppingCartRouter.post('/createTransaction', shoppingCartController.createTransaction);
 
 module.exports = shoppingCartRouter
--- controller/shoppingCartController.js
+++ controller/shoppingCartController.js
@@ -5,8 +5,8 @@ const { PaymentRequest, PaymentResponse } = require('../model/transaction');
 const config = require('config');
 
 const createShoppingCartObj = (req, token) => ({
-  price: req.query.price,
-  quantity: req.query.quantity,
+  price: req.body.price,
+  quantity: req.body.quantity,
   oneTimeToken: token,
   securePanelUrl: config.securePanelUrl,
 });
@@ -16,11 +16,13 @@ const getTokens = async () => {
   const headers = {
     'x-api-key': global.xApiKey,
   };
-
+  console.log("Create OneTimeToken request Start with RequestBody: " + JSON.stringify(tokensRequest));
   try {
     const response = await axios.post(`${config.singleApiBaseUrl}/tokens`, tokensRequest, { headers });
+    console.log("Create OneTimeToken reques finished with ResponseBody: " + JSON.stringify(response.data));
     return new TokensResponse(response.data);
   } catch (error) {
+    console.log("An Error Occured during create transaction request with: "  + error);
     throw error;
   }
 };
@@ -42,14 +44,24 @@ const createTransaction = async (req, res) => {
     'x-api-key': global.xApiKey,
     'x-idempotency-key': randomstring.generate(35),
   };
+  const xApiKey =  global.xApiKey;
+  console.log("The length of the API Key is: " + xApiKey.length);
+  console.log("The first 6 of API Key is: " + xApiKey.substring(0, 6) +
+  " The last 4 of API Key is: " + xApiKey.substring(xApiKey.length - 4, xApiKey.length));
+  console.log("Create transaction request start with RequestBody: " + JSON.stringify(paymentRequest));
 
   try {
     const response = await axios.post(`${config.singleApiBaseUrl}/transactions`, paymentRequest, { headers });
     const paymentResponse = new PaymentResponse(response.data);
-        res.render('response', { paymentResponse });
+    console.log("Create transaction request finished with ResponseBody: " + JSON.stringify(paymentResponse));
+    res.render('response', { paymentResponse });
   } catch (error) {
-        const result = error.response.data.result;
-        res.render('response', { paymentResponse: { result } });
+      console.log("An Error Occured during create transaction request with: " + JSON.stringify(error));
+      var errorMessage = "Unknow Error";
+      if(error.response != undefined){
+          errorMessage = error.response.status
+      }
+      res.render('response', { paymentResponse: { errorMessage } });
   }
 };
 
