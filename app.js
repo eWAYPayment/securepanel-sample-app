a832de1a30ca009ae4fd125d7d7782a521dbaae0
Lily Yang <lily.yang@globalpay.com>
2025-06-10 14:51:36 +0800

    upgrade sample app to v1.1.0

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require("cors");   
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const config = require('config');
 app.set('view engine', 'ejs');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const port = config.port || 3000
+const port = config.port || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 app.use((req, res, next) =>{
@@ -26,11 +26,11 @@ app.use((req, res, next) =>{
 
 global.xApiKey = config.xApiKey;
 
-const indexRouter = require('./Routes/indexRouter') 
-app.use(indexRouter)
+const indexRouter = require('./Routes/indexRouter');
+app.use(indexRouter);
 
-const shoppingCartRouter = require('./Routes/shoppingCartRouter') 
-app.use(shoppingCartRouter)
+const shoppingCartRouter = require('./Routes/shoppingCartRouter');
+app.use(shoppingCartRouter);
 
 
 app.listen(port, function() {
--- public/js/shoppingCart.js
+++ public/js/shoppingCart.js
@@ -1,97 +1,116 @@
+var maxPhoneSize = 768;
 $(document).ready(function () {
+    $('#submit-secure-panel').prop('disabled', true);
     checkWindowSize();
     // Configuration for SecurePanel
     var config = {
-        oneTimeToken: oneTimeToken,
+        oneTimeToken: $('#oneTimeToken').val(),
         fieldDivId: "secure-panel",
         layout: {
             rows: [{
-                    classes: "bg-primary text-white",
-                    cells: [{
-                            colspan: 6,
-                            label: {
-                                text: "Card Name:",
-                                fieldColspan: 5
-                            },
-                            field: {
-                                fieldType: "name",
-                                fieldColspan: 7
-                            }
-                        },
-                        {
-                            colspan: 6,
-                            label: {
-                                text: "Card Number:",
-                                fieldColspan: 5
-                            },
-                            field: {
-                                fieldType: "number",
-                                fieldColspan: 7
-                            }
-                        }
-                    ],
+                classes: "bg-primary text-white",
+                cells: [{
+                    colspan: 6,
+                    label: {
+                        text: "Card Number:",
+                        fieldColspan: 5
+                    },
+                    field: {
+                        fieldType: "number",
+                        fieldColspan: 7
+                    }
+                }
+                    , {
+                    colspan: 6,
+                    label: {
+                        text: "Card Name:",
+                        fieldColspan: 5
+                    },
+                    field: {
+                        fieldType: "name",
+                        fieldColspan: 7
+                    }
+                }
+                ],
+            },
+            {
+                classes: "bg-primary text-white",
+                cells: [{
+                    colspan: 6,
+                    label: {
+                        text: "Expiry:",
+                        fieldColspan: 5
+                    },
+                    field: {
+                        fieldType: "expiry",
+                        fieldColspan: 7
+                    }
                 },
                 {
-                    classes: "bg-primary text-white",
-                    cells: [{
-                            colspan: 6,
-                            label: {
-                                text: "Expiry:",
-                                fieldColspan: 5
-                            },
-                            field: {
-                                fieldType: "expiry",
-                                fieldColspan: 7
-                            }
-                        },
-                        {
-                            colspan: 6,
-                            label: {
-                                text: "CVV Number:",
-                                fieldColspan: 5
-                            },
-                            field: {
-                                fieldType: "cvn",
-                                fieldColspan: 7
-                            }
-                        }
-                    ],
-                },
+                    colspan: 6,
+                    label: {
+                        text: "CVV Number:",
+                        fieldColspan: 5
+                    },
+                    field: {
+                        fieldType: "cvn",
+                        fieldColspan: 7
+                    }
+                }
+                ],
+            },
             ],
         },
     };
 
+    var initialiseModel = { amount: Number.parseFloat($('#amount').val()) * 100 };
+
     // Callback function for initialization
     function initializeCallback(response) {
         console.log("Initialization response:", response);
+        $('#submit-secure-panel').prop('disabled', false);
+    }
+
+    // Callback function for surcharge
+    function surchargeCallback(response, data) {
+        console.log("Surcharge response:", response, data);
+        if (response.result.status == "ok" && Number.parseInt(data?.surcharge) != Number.NaN) {
+            // update surcharge and total amount:
+            var surcharge = parseFloat(data.surcharge / 100);
+            var totalAmount = surcharge + parseFloat($("#price").val()) * parseFloat($("#quantity").val());
+            $("#lbl_surcharge").text(`$ ${surcharge.toFixed(2)}`);
+            $("#surcharge").val(surcharge);
+            $("#totalGST").text(`$ ${totalAmount.toFixed(2)}`);
+            $("#amount").val(totalAmount);
+        }
     }
 
     // Callback function for processing
     function processCallback(response, data) {
         console.log("Processing response:", response);
         console.log("Processing data:", data);
-        if(data != null && data.canCreateTransaction){
+        if (data != null && data.canCreateTransaction) {
             $("#createTransactionForm").submit();
-        }else{
-            showPopUpWindow(response);            
+        } else {
+            showPopUpWindow(response);
             $('#submit-secure-panel').html('Pay');
-            $('#submit-secure-panel').prop('disabled', false);            
+            $('#submit-secure-panel').prop('disabled', false);
         }
-        
     }
+
     // Initialize SecurePanel on document ready (equivalent to window load)
     $(function () {
-        GPAUNZ.SecurePanel.initialise(config, null, initializeCallback);
+        GPAUNZ.SecurePanel.initialise(config, initialiseModel, initializeCallback, null, $("#lbl_surcharge").length > 0 ? surchargeCallback: undefined);
     });
 
     function showPopUpWindow(response) {
         console.log(response);
-        if(response.result && (response.result.status === "ok" || response.result.status === "approved")) {
+        if (response.result && (response.result.status === "ok" || response.result.status === "approved")) {
             Swal.fire({
                 title: 'Processed Successfully!',
-                text: 'Transaction id: '+ response.id,
+                text: 'Transaction id: ' + response.id,
                 icon: 'success',
-                heightAuto: false                
+                heightAuto: false
             }).then((result) => {
                 if (result.isConfirmed) {
                     window.location = "/"; // Use "window.location" to redirect
@@ -110,7 +129,7 @@ $(document).ready(function () {
                     case "V10088":
                         return "Invalid expiry date";
                     default:
-                        return "Unknown error with ID " + code.id + ": " + code.message;
+                        return "An error occurred with error ID " + code.id + ": " + code.message;
                 }
             });
             Swal.fire({
@@ -145,8 +164,18 @@ $(document).ready(function () {
             $('#footer').css("visibility", "visible");
         }
     };
-    
+
     $(window).resize(function () {
         checkWindowSize();
     });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    function displayCurrentYear() {
+        const currentYearElement = document.getElementById('current-year');
+        if (currentYearElement) {
+            currentYearElement.textContent = new Date().getFullYear();
+        }
+    }
+    displayCurrentYear();
 });
